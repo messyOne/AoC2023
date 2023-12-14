168b18467b6764050dd7213e3728ab09ce6dad7d
daniel.weiss <daniel.weiss@innogames.com>
2023-12-14 12:25:17 +0100

    Day04-2

--- src/main/kotlin/days/Day04.kt
+++ src/main/kotlin/days/Day04.kt
@@ -21,11 +21,32 @@ class Day04 : Day {
     }
 
     override fun executePart2(): Long {
-        return 0
+        val cards = File("src/main/resources/day04-2.txt").useLines { line ->
+            line.toList().map { line ->
+                val split1 = line.split(":")
+                val split2 = split1.last().split("|")
+                val id = split1.first().split(" ").last().toInt()
+                val winningNumbers = split2.first().split(" ").filter { it.trim().isNotEmpty() }.map { it.toInt() }
+                val numbers = split2.last().split(" ").filter { it.trim().isNotEmpty() }.map { it.toInt() }
+
+                Card(id, winningNumbers, numbers)
+            }
+        }
+
+        for (i in cards.indices) {
+            for (x in 0 until cards[i].counter) {
+                for (j in i + 1..i + cards[i].getWins()) {
+                    cards[j].increaseCounter()
+                }
+            }
+
+        }
+
+        return cards.sumOf { it.counter.toLong() }
     }
 }
 
-data class Card(val id: Int, val winningNumbers: List<Int>, val numbers: List<Int>) {
+data class Card(val id: Int, val winningNumbers: List<Int>, val numbers: List<Int>, var counter: Int = 1) {
     fun getScore(): Long {
         val wins = numbers.intersect(winningNumbers.toSet()).count()
 
@@ -38,5 +59,13 @@ data class Card(val id: Int, val winningNumbers: List<Int>, val numbers: List<In
             start *= 2
         }
         return start
+    }
+
+    fun getWins(): Int {
+        return numbers.intersect(winningNumbers.toSet()).count()
+    }
+
+    fun increaseCounter() {
+        counter++
     }
 }
